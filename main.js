82c7dee4e0ddea5ea076d87eed5624b799bd6844
payal-12-3 <payaldholakiya312@gmail.com>
2021-02-20 14:59:37 +0530

    remove comment

--- main.js
+++ main.js
@@ -5,9 +5,7 @@ function myFunction() {
   var li = ul.getElementsByTagName('li')
   for (i = 0; i < li.length; i++) {
     var a = li[i].getElementsByTagName('a')[0]
-    // var txtValue = a.textContent || a.innerText
     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      console.log(a.innerHTML.toUpperCase().indexOf(filter) > -1)
       li[i].style.display = ''
     } else {
       li[i].style.display = 'none'
